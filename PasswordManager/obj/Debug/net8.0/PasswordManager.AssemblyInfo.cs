4b8304718650582a6b4399e8da7386b4936ab956
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2024-05-28 01:28:57 -0700

    Adjusted password generator filter to account for when a user deselects all options

--- PasswordManager/obj/Debug/net8.0/PasswordManager.AssemblyInfo.cs
+++ PasswordManager/obj/Debug/net8.0/PasswordManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13173ef7b204f637731c525542b71205e925976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4d8f2f806ec74b74d1275add59e234c7c4392a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
